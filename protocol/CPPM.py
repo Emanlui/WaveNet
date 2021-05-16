2b4e943270e2be78019dd437f80fb8375f48d832
Marco <imarco.venegas@gmail.com>
2021-05-15 20:16:50 -0600

    Added custom protocol and protocol encrypt/decrypt

---
+++ protocol/CPPM.py
@@ -0,0 +1,24 @@
+from scapy.all import *
+from scapy.fields import *
+from scapy.layers.inet import IP,TCP
+import struct
+
+class CPPM(Packet):
+    #Communication Protocol Packet Manager
+    name = "CPPM"
+
+    fields_desc = [
+        LenField('messageLength', 0, fmt="H"),
+        ShortField("chksum", None),
+        ShortField("version", 1),
+        StrField("message", "")
+    ]
+    
+    def post_build(self, p, pay):
+        p += pay
+        self.chksum = checksum(p)
+        if(self.chksum == None):
+            self.chksum = checksum(p)
+            print(self.chksum)
+        return p 
+    
--- server.py
+++ server.py
@@ -1,39 +1,12 @@
 import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+logging.getLogger("scapy.runtime").setLevel(logging.INFO)
 from Protocol import *
+import libpcap
 
-# SOCKET SETTINGS
-############################################################
+
 TCP_IP = '172.17.0.2'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-############################################################
-
-# NAMES TO SEND INSIDE THE RESPONSE
-############################################################
-SERVER_NAMES = {
-    "1": "Serv1",
-    "2": "Serv2",
-    "3": "Serv3",
-    "4": "Serv4"
-}
-############################################################
-
-
-# FUNCTION TO RETURN THE NAME
-############################################################
-def get_the_name(layer):
-    return_name = SERVER_NAMES[layer.sprintf("%name_number%")]
-    return return_name
-############################################################
-
-# DICTIONARY WITH THE POSSIBLES ACTIONS
-############################################################
-actions = {
-    "get_the_name": get_the_name,
-}
-############################################################
-
+BUFFER_SIZE = 1024  
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,14 +18,15 @@ def main():
         print('Connection address: {}'.format(addr))
         try:
             data = conn.recv(BUFFER_SIZE)
+            #packets = sniff(filter="dst port 5005", count=5)   
+
             if data:
-                received_packet = Ether() / IP() / TCP() / Protocol(data)
-                required_name = actions[received_packet.getlayer("Protocol").sprintf("%service%")] \
-                    (received_packet.getlayer("ProtocolRequest"))
-                print(received_packet.show())
-                print(required_name)
-            #packet_to_send = str(Protocol(direction=1, service=1) / ProtocolResponse(own_name=required_name))
-            #conn.sendall(packet_to_send)
+
+                sa = SecurityAssociation(ESP, spi=0xdeadbeef, crypt_algo='AES-CBC', crypt_key=str('2948404D63516654').encode())
+                decrypted_packet = sa.decrypt(IP(data))
+                received_packet = CPPM(decrypted_packet.getlayer(Raw).load)
+                received_packet.show2()
+
             else:
                 pass
         except Exception as server_error:
